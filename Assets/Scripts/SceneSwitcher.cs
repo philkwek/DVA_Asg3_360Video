3e28f529f4a8beb242fda9eb30f589b7e302986d
philkwek <philkwek03@gmail.com>
2022-08-01 16:47:36 +0800

    added hotspot teleports for energy story exhibit

--- Assets/Scripts/SceneSwitcher.cs
+++ Assets/Scripts/SceneSwitcher.cs
@@ -2,19 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class SceneSwitcher : MonoBehaviour
 {
     public Animator transition;
+    public bool isWalkthrough;
+    public string newSceneName;
+    private SceneSwitcher sceneSwitcher;
+    [SerializeField] private VideoPlayer videoPlayer;
 
     public void Start()
     {
-        transition = FindObjectOfType<Animator>();
+        videoPlayer = FindObjectOfType<VideoPlayer>();
+        sceneSwitcher = FindObjectOfType<SceneSwitcher>();
+        videoPlayer.loopPointReached += EndReached;
+
+        Animator[] objects = FindObjectsOfType<Animator>();
+        for (int i = 0; i < objects.Length; i++)
+        {
+            if (objects[i].gameObject.name == "Image")
+            {
+                transition = objects[i];
+                return;
+            }
+        }
     }
 
     public void SwitchScene(string sceneName)
     {
+        Debug.Log("Switching Scenes");
         StartCoroutine(Switch(sceneName));
+    }
+
+    void EndReached(VideoPlayer vp)
+    {
+        if (isWalkthrough)
+        {
+            sceneSwitcher.SwitchScene(newSceneName);
+        }
     }
 
     IEnumerator Switch(string scene)
@@ -22,7 +48,7 @@ public class SceneSwitcher : MonoBehaviour
         Debug.Log("Switching Scene");
         transition.SetTrigger("Start");
 
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(2f);
 
         SceneManager.LoadScene(scene);
     }

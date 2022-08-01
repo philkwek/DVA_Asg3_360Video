e3f386199750a3b5fb0632f36cc156c70ca42b90
philkwek <philkwek03@gmail.com>
2022-08-01 12:19:13 +0800

    push

--- Assets/Scripts/LookAroundScript.cs
+++ Assets/Scripts/LookAroundScript.cs
@@ -5,14 +5,33 @@ using UnityEngine;
 public class LookAroundScript : MonoBehaviour
 {
     public float speed = 3;
+    public float zoomSensitivity = 1.5f;
+    public Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = FindObjectOfType<Camera>();
+    }
 
     // Update is called once per frame
     void Update()
     {
+        //Camera Look around
         if (Input.GetMouseButton(0))
             {
                 transform.RotateAround(transform.position, -Vector3.up, speed * Input.GetAxis("Mouse X"));
                 transform.RotateAround(transform.position, transform.right, speed * Input.GetAxis("Mouse Y"));
             };
+
+        //Camera zooming 
+        mainCamera.fieldOfView += zoomSensitivity * Input.mouseScrollDelta.y;
+        if (mainCamera.fieldOfView > 100)
+        {
+            mainCamera.fieldOfView = 100;
+        };
+        if (mainCamera.fieldOfView < 10)
+        {
+            mainCamera.fieldOfView = 10;
+        }
     }
 }
